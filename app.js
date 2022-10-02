b1b4b90e6c85e3733992e5cd12b601b641277017
Julien Thibault <julien.thibault@live.fr>
2022-10-02 17:13:44 +0200

    JS

--- app.js
+++ app.js
@@ -1,104 +1,132 @@
-function showValidation({index, validation}) {
-    if(validation){
-      validationIcons[index].style.display = "inline";
-      validationIcons[index].src = "ressources/check.svg";
-      if(validationTexts[index])  validationTexts[index].style.display = "none";
-    } 
-    else {
-      validationIcons[index].style.display = "inline";
-      validationIcons[index].src = "ressources/error.svg";
-      if(validationTexts[index]) validationTexts[index].style.display = "block";
+const inputsValidity = {
+    user: false,
+    email: false,
+    password: false,
+    passwordConfirmation: false
+};
+
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
+
+let isAnimating = false;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keys = Object.keys(inputsValidity);
+    const failedInputs = keys.filter(key => !inputsValidity[key]);
+    if (failedInputs.length && !isAnimating) {
+        isAnimating = true;
+        container.classList.add("shake");
+        setTimeout(() => {
+            container.classList.remove("shake");
+            isAnimating = false;
+        }, 400);
+        failedInputs.forEach(input => {
+            const index = keys.indexOf(input);
+            showValidation({ index: index, validation: false });
+        });
+    } else {
+        alert("Données envoyées avec succès !");
+        window.location.reload();
     }
-  }
+});
+
+
+function showValidation({ index, validation }) {
+    if (validation) {
+        validationIcons[index].style.display = "inline";
+        validationIcons[index].src = "ressources/check.svg";
+        if (validationTexts[index])
+            validationTexts[index].style.display = "none";
+    } else {
+        validationIcons[index].style.display = "inline";
+        validationIcons[index].src = "ressources/error.svg";
+        if (validationTexts[index])
+            validationTexts[index].style.display = "block";
+    }
+}
 
 const validationIcons = document.querySelectorAll(".icone-verif");
 const validationTexts = document.querySelectorAll(".error-msg");
 
-const userInput = document.querySelector(".input-group:nth-child(1) input")
+const userInput = document.querySelector(".input-group:nth-child(1) input");
 
-userInput.addEventListener("blur", userValidation)
-userInput.addEventListener("input", userValidation)
+userInput.addEventListener("blur", userValidation);
+userInput.addEventListener("input", userValidation);
 
-function userValidation(){
-  if(userInput.value.length >= 3) {
-    showValidation({index: 0, validation: true})
-    inputsValidity.user = true;
-  }
-  else {
-    showValidation({index: 0, validation: false})
-    inputsValidity.user = false;
-  }
+function userValidation() {
+    if (userInput.value.length >= 3) {
+        showValidation({ index: 0, validation: true });
+        inputsValidity.user = true;
+    } else {
+        showValidation({ index: 0, validation: false });
+        inputsValidity.user = false;
+    }
 }
 
-const mailInput = document.querySelector(".input-group:nth-child(2) input")
+const mailInput = document.querySelector(".input-group:nth-child(2) input");
 
-mailInput.addEventListener("blur", mailValidation)
-mailInput.addEventListener("input", mailValidation)
+mailInput.addEventListener("blur", mailValidation);
+mailInput.addEventListener("input", mailValidation);
 
-const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function mailValidation(){
-  if(regexEmail.test(mailInput.value)){
-    showValidation({index: 1, validation: true})
-    inputsValidity.email = true;
-
-  }
-  else {
-    showValidation({index: 1, validation: false})
-    inputsValidity.email = false;
-
-  }
+function mailValidation() {
+    if (regexEmail.test(mailInput.value)) {
+        showValidation({ index: 1, validation: true });
+        inputsValidity.email = true;
+    } else {
+        showValidation({ index: 1, validation: false });
+        inputsValidity.email = false;
+    }
 }
 
-const pswInput = document.querySelector(".input-group:nth-child(3) input")
+const pswInput = document.querySelector(".input-group:nth-child(3) input");
 
-pswInput.addEventListener("blur", passwordValidation)
-pswInput.addEventListener("input", passwordValidation)
+pswInput.addEventListener("blur", passwordValidation);
+pswInput.addEventListener("input", passwordValidation);
 
 const passwordVerification = {
-  length: false,
-  symbol: false,
-  number: false
-}
+    length: false,
+    symbol: false,
+    number: false,
+};
 
 const regexList = {
-  symbol: /[^a-zA-Z0-9\s]/,
-  number: /[0-9]/
-}
+    symbol: /[^a-zA-Z0-9\s]/,
+    number: /[0-9]/,
+};
 
 let passwordValue;
 
-function passwordValidation(){
-  passwordValue = pswInput.value;
-  let validationResult = 0;
+function passwordValidation() {
+    passwordValue = pswInput.value;
+    let validationResult = 0;
 
-  for(const prop in passwordVerification){
-    
-    if(prop === "length") {
-      if(passwordValue.length < 6) {
-        passwordVerification.length = false;
-      }
-      else {
-        passwordVerification.length = true;
-        validationResult++;
-      }
-      continue;
-    }
-
-    if(regexList[prop].test(passwordValue)) {
-      passwordVerification[prop] = true;
-        validationResult++;
-    } 
-    else {
-      passwordVerification[prop] = false;
-    }
-  }
-  
-    if(validationResult !== 3) {
-        showValidation({index: 2, validation: false})
+    for (const prop in passwordVerification) {
+        if (prop === "length") {
+            if (passwordValue.length < 6) {
+                passwordVerification.length = false;
+            } else {
+                passwordVerification.length = true;
+                validationResult++;
+            }
+            continue;
         }
-    else {
-        showValidation({index: 2, validation: true})
+
+        if (regexList[prop].test(passwordValue)) {
+            passwordVerification[prop] = true;
+            validationResult++;
+        } else {
+            passwordVerification[prop] = false;
+        }
+    }
+
+    if (validationResult !== 3) {
+        showValidation({ index: 2, validation: false });
+        inputsValidity.password = false;
+    } else {
+        showValidation({ index: 2, validation: true });
+        inputsValidity.password = true;
     }
 
     passwordStrength();
@@ -106,30 +134,54 @@ function passwordValidation(){
 
 const lines = document.querySelectorAll(".lines div");
 
-function passwordStrength(){
+function passwordStrength() {
     const passwordLength = pswInput.value.length;
 
-    if(!passwordLength) {
+    if (!passwordLength) {
         addLines(0);
-    } 
-    else if(passwordLength > 9 && passwordVerification.number && passwordVerification.symbol) {
+    } else if (
+        passwordLength > 9 &&
+        passwordVerification.number &&
+        passwordVerification.symbol
+    ) {
         addLines(3);
-    }
-    else if(passwordLength > 6 && (passwordVerification.number || passwordVerification.symbol)) {
+    } else if (
+        passwordLength > 6 &&
+        (passwordVerification.number || passwordVerification.symbol)
+    ) {
         addLines(2);
-    }
-    else {
+    } else {
         addLines(1);
+    }
+
+    function addLines(numberOfLines) {
+        lines.forEach((line, index) => {
+            if (index < numberOfLines) {
+                line.style.display = "block";
+            } else {
+                line.style.display = "none";
+            }
+        });
+    }
+    if (validationIcons[3].style.display === "inline") {
+        confirmPassword();
     }
 }
 
-function addLines(numberOfLines){
-    lines.forEach((line, index) => {
-        if(index < numberOfLines) {
-            line.style.display = "block";
-        } 
-        else {
-            line.style.display = "none";
-        }
-    })
+const confirmInput = document.querySelector(".input-group:nth-child(4) input");
+
+confirmInput.addEventListener("blur", confirmPassword);
+confirmInput.addEventListener("input", confirmPassword);
+
+function confirmPassword() {
+    const confirmValue = confirmInput.value;
+    if (!confirmValue && !passwordValue) {
+        validationIcons[3].style.display = "none";
+    } else if (confirmValue !== passwordValue) {
+        showValidation({ index: 3, validation: false });
+        inputsValidity.passwordConfirmation = false;
+    } else {
+        showValidation({ index: 3, validation: true });
+        inputsValidity.passwordConfirmation = true;
+    }
 }
